1ecc07b0c088b7849feaf6681fec63d91d4995c3
Thor J. Kooda <792089+tkooda@users.noreply.github.com>
2026-04-06 21:32:57 -0500

    fix build error

--- app/src/main/java/com/example/zwatchdog/AlarmReceiver.java
+++ app/src/main/java/com/example/zwatchdog/AlarmReceiver.java
@@ -11,7 +11,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.widget.Toast;
-import com.example.zwatchdog.BuildConfig;
 
 public class AlarmReceiver extends BroadcastReceiver {
     private static final String TARGET_PKG = "com.loudtalks";
